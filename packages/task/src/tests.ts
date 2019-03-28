90b2424db0d94a9c2e41849441de864f2e14528d
Ben Newman <ben@eloper.dev>
2019-03-28 12:52:10 -0400

    Add a basic test of history tracking.

--- packages/task/src/tests.ts
+++ packages/task/src/tests.ts
@@ -1,8 +1,27 @@
 import assert from "assert";
-import { Task } from "./task";
+import { Task, bindHistory } from "./task";
 
 describe("Task", function () {
   it("should be importable", function () {
     assert.strictEqual(typeof Task, "function");
+  });
+
+  it("tracks history correctly", function () {
+    let parentTask: Task<any>;
+    return new Task(task => {
+      parentTask = task;
+      task.resolve(123);
+    }).then(oneTwoThree => new Task(child => {
+      assert.strictEqual(child.history.parent, parentTask.history);
+      setTimeout(bindHistory(() => {
+        child.resolve(new Task(grandchild => {
+          assert.strictEqual(grandchild.history.parent, child.history);
+          assert.strictEqual(grandchild.history.parent!.parent, parentTask.history);
+          grandchild.resolve(oneTwoThree);
+        }));
+      }), 10);
+    })).then(result => {
+      assert.strictEqual(result, 123);
+    });
   });
 });
