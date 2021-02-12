aa4bd6eabe21d945c1696a3428284e10b4e53aae
Ben Newman <ben@benjamn.com>
2021-02-12 17:03:17 -0500

    Test canonization of acyclic nested arrays.

--- packages/canon/src/tests.ts
+++ packages/canon/src/tests.ts
@@ -14,4 +14,36 @@ describe("Canon", () => {
     assert.strictEqual(canon.admit(false), false);
     assert.strictEqual(canon.admit(null), null);
   });
+
+  it("can admit simple nested arrays", () => {
+    const canon = new Canon;
+
+    const input = [
+      "a",
+      ["b", ["c", "d"]],
+      [1, 2, 3],
+      "f",
+    ];
+
+    const admitted1 = canon.admit(input);
+    assert.deepStrictEqual(input, admitted1);
+    const admitted2 = canon.admit(input);
+    assert.deepStrictEqual(input, admitted2);
+    assert.strictEqual(admitted1, admitted2);
+
+    const cd = canon.admit(input[1][1]);
+    assert.deepStrictEqual(cd, ["c", "d"]);
+    assert.strictEqual(cd, admitted1[1][1]);
+    assert.strictEqual(cd, admitted2[1][1]);
+
+    assert.strictEqual(
+      canon.admit(admitted1),
+      admitted2,
+    );
+
+    assert.strictEqual(
+      canon.admit(admitted2),
+      admitted1,
+    );
+  });
 });
