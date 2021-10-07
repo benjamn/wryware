1135e70bdce27aa9882bda1a07f8ddacc322955d
Ben Newman <ben@benjamn.com>
2021-10-07 17:37:57 -0400

    Trivial a === b shortcut for top-level equal(a, b) function.

--- packages/equality/src/equality.ts
+++ packages/equality/src/equality.ts
@@ -4,7 +4,7 @@ import { DeepChecker } from "./checker";
  * Performs a deep equality check on two JavaScript values, tolerating cycles.
  */
 export function equal(a: any, b: any): boolean {
-  return new DeepChecker().check(a, b);
+  return a === b || new DeepChecker().check(a, b);
 }
 
 // Allow default imports as well.
