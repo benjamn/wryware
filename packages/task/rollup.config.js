c41e729dc7d05f0459cdee064a1c9a7c14210442
Ben Newman <ben@benjamn.com>
2023-05-02 16:34:23 -0400

    Convert `@wry/task` to new tsc/rollup build strategy.

--- packages/task/rollup.config.js
+++ packages/task/rollup.config.js
@@ -1,6 +1,3 @@
-import typescriptPlugin from 'rollup-plugin-typescript2';
-import typescript from 'typescript';
-
 const globals = {
   __proto__: null,
   tslib: "tslib",
@@ -11,31 +8,33 @@ function external(id) {
   return id in globals;
 }
 
-export default [{
-  input: "src/task.ts",
-  external,
-  output: {
-    file: "lib/task.esm.js",
-    format: "esm",
-    sourcemap: true,
-    globals,
-  },
-  plugins: [
-    typescriptPlugin({
-      typescript,
-      tsconfig: "./tsconfig.rollup.json",
-    }),
-  ],
-}, {
-  input: "lib/task.esm.js",
-  external,
-  output: {
-    // Intentionally overwrite the task.js file written by tsc:
-    file: "lib/task.js",
-    format: "cjs",
-    exports: "named",
-    sourcemap: true,
-    name: "task",
-    globals,
-  },
-}];
+function build(input, output, format) {
+  return {
+    input,
+    external,
+    output: {
+      file: output,
+      format,
+      sourcemap: true,
+      globals
+    },
+  };
+}
+
+export default [
+  build(
+    "lib/es5/index.js",
+    "lib/bundle.cjs",
+    "cjs"
+  ),
+  build(
+    "lib/tests/main.js",
+    "lib/tests/bundle.js",
+    "esm"
+  ),
+  build(
+    "lib/es5/tests/main.js",
+    "lib/tests/bundle.cjs",
+    "cjs"
+  ),
+];
