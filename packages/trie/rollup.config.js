8f39a4c6820bf5284d362506aea95358f65a915f
Ben Newman <ben@benjamn.com>
2023-05-02 16:25:11 -0400

    Convert `@wry/trie` to new tsc/rollup build strategy.

--- packages/trie/rollup.config.js
+++ packages/trie/rollup.config.js
@@ -1,40 +1,43 @@
-import typescriptPlugin from 'rollup-plugin-typescript2';
-import typescript from 'typescript';
-
 const globals = {
   __proto__: null,
   tslib: "tslib",
+  "@wry/context": "wryContext",
+  "@wry/trie": "wryTrie",
+  "@wry/task": "wryTask",
+  "@wry/equality": "wryEquality",
 };
 
 function external(id) {
   return id in globals;
 }
 
-export default [{
-  input: "src/trie.ts",
-  external,
-  output: {
-    file: "lib/trie.esm.js",
-    format: "esm",
-    sourcemap: true,
-    globals,
-  },
-  plugins: [
-    typescriptPlugin({
-      typescript,
-      tsconfig: "./tsconfig.rollup.json",
-    }),
-  ],
-}, {
-  input: "lib/trie.esm.js",
-  external,
-  output: {
-    // Intentionally overwrite the trie.js file written by tsc:
-    file: "lib/trie.js",
-    format: "cjs",
-    exports: "named",
-    sourcemap: true,
-    name: "trie",
-    globals,
-  },
-}];
+function build(input, output, format) {
+  return {
+    input,
+    external,
+    output: {
+      file: output,
+      format,
+      sourcemap: true,
+      globals
+    },
+  };
+}
+
+export default [
+  build(
+    "lib/es5/index.js",
+    "lib/bundle.cjs",
+    "cjs"
+  ),
+  build(
+    "lib/tests/main.js",
+    "lib/tests/bundle.js",
+    "esm"
+  ),
+  build(
+    "lib/es5/tests/main.js",
+    "lib/tests/bundle.cjs",
+    "cjs"
+  ),
+];
